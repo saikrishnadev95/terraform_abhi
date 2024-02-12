10a44d8c423a10412be57865a133f85bb1cd911b
ChaitanyaChanddra <chay@outlook.in>
2024-02-12 22:28:31 +0530

    changes

---
+++ d3/main.tf
@@ -0,0 +1,21 @@
+variable "ami_value" {
+  description = "value for the ami"
+}
+
+variable "instance_type_value" {
+  description = "value for instance_type"
+}
+
+variable "subnet_id_value" {
+  description = "value for the subnet_id"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test" {
+  ami = "var.ami_value"
+  instance_type = "var.instance_type_value"
+  subnet_id = "var.subnet_id_value"
+}
---
+++ d3/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-0f3c7d07486cad139"
+instance_type_value = "t2.micro"
+subnet_id_value = "	subnet-00ec21ac0a060c0f4"
