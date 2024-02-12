5b002dcd633375e9736374185a651b17fe54a1e4
ChaitanyaChanddra <chay@outlook.in>
2024-02-12 22:39:16 +0530

    changes

--- d3/main.tf
+++ d3/main.tf
@@ -1,15 +1,3 @@
-variable "ami_value" {
-  description = "value for the ami"
-}
-
-variable "instance_type_value" {
-  description = "value for instance_type"
-}
-
-variable "subnet_id_value" {
-  description = "value for the subnet_id"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
