d56f8abc644e69372fa78ebc2954ff0964b2f02d
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-02-14 16:55:28 +0530

    changes

--- d4/main.tf
+++ d4/main.tf
@@ -11,3 +11,14 @@ resource "aws_instance" "test" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "tsk-s3-bucket-35"
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "terraform_lock"
+  billing_mode = "pay_per_request"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
