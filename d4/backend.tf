51f02b25ca3268f1660927a1a2a15072032197ab
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-02-14 16:33:10 +0530

    changes

--- d4/backend.tf
+++ d4/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "tsk-s3-bucket-36"
     region = "us-east-1"
-    key = "/home/centos/terraform_abhi/d4"
+    key = "home_centos_terraform_abhi_d4/terraform.tfstate"
   }
 }
