76ad8b23f793a596b3f7e557b771122bd0253116
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-02-15 15:58:41 +0530

    changes

--- d4/main.tf
+++ d4/main.tf
@@ -8,17 +8,17 @@ resource "aws_instance" "test" {
   subnet_id = "subnet-00ec21ac0a060c0f4"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "tsk-s3-bucket-35"
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name = "terraform_lock"
-  billing_mode = "pay_per_request"
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#resource "aws_s3_bucket" "bucket" {
+#  bucket = "tsk-s3-bucket-35"
+#}
+#
+#resource "aws_dynamodb_table" "terraform_lock" {
+#  name = "terraform_lock"
+#  billing_mode = "pay_per_request"
+#  hash_key = "LockID"
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
